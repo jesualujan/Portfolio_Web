ad25e9de73fe133ae19649d572ea30634629b742
jesualujan <lujan.oficial@gmail.com>
2025-03-03 20:42:54 -0600

    ✨ configuración de rutas

--- portfolio_web/vite.config.js
+++ portfolio_web/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // añado el path para poder saber la ruta
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // configuramos alias para mis carpetas @
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 })
