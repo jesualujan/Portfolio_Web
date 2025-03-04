5b5672551ffb524363636b38d30b750bed227c73
jesualujan <lujan.oficial@gmail.com>
2025-03-03 20:43:06 -0600

    ✨ desarrollo de app

--- portfolio_web/src/App.jsx
+++ portfolio_web/src/App.jsx
@@ -1,13 +1,33 @@
+import { ThemeProvider } from 'styled-components';
+import { useState } from 'react';
+import styled from 'styled-components';
+import Navbar from '@/Components/Navbar/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { darkTheme, lightTheme } from '@/Utils/Themes';
 
+// Estilos para el contenedor principal
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+  min-height: 100vh; /* Para asegurar que el cuerpo cubra toda la altura de la ventana */
+`;
 
 function App() {
-  
+  const [darkMode, setDarkMode] = useState(true);
 
   return (
-    <>
-<h1>Hello World</h1>
-    </>
-  )
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router>
+        <Navbar />
+        <Body>
+
+        </Body>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
+
+
