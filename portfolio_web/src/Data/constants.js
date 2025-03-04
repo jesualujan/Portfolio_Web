b1c355fd4de797cf96dc60206be8a6838ca48095
jesualujan <lujan.oficial@gmail.com>
2025-03-03 20:43:30 -0600

    ✨ creación de la data

---
+++ portfolio_web/src/Data/constants.js
@@ -0,0 +1,17 @@
+export const Bio = {
+  name: "Jesua Hadai",
+  roles: [
+    "Full Stack Developer",
+    "Code Sensei",
+    "Teacher",
+    "IT Manager",
+  ],
+    description:
+    "Self-taught, passionate about teaching and technological inclusion, resident of the state of Jalisco. Entrepreneur, enthusiastic about learning and solving problems with technology. Mexican web developer with more than 5 years of experience as a Code Sensei guiding students on their way to technological success for their professional preparation as web developers in most of Latin America.",
+  github: "https://github.com/jesualujan",
+  resume:
+    "https://docs.google.com/document/d/19kOur-8hQlApR7PINDQLjb8jdUL3JTJK/edit?usp=sharing&ouid=102157425828694167404&rtpof=true&sd=true",
+  linkedin: "https://www.linkedin.com/in/jesua-hadai-alderete-luj%C3%A1n-a2325a121/",
+  facebook: "https://www.facebook.com/profile.php?id=100093507976508&mibextid=gik2fB",
+  notion: "https://www.notion.so/Personal-Portfolio-21ecba0082284e18a7dd154a734b5c9c?pvs=4",
+};
