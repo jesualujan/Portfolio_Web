59bf78d906f7214cc0df132af41e675613c9b1e7
jesualujan <lujan.oficial@gmail.com>
2025-03-03 20:42:32 -0600

    ✨ configuración de standard

--- portfolio_web/eslint.config.js
+++ portfolio_web/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import standard from 'eslint-config-standard'
 
 export default [
   { ignores: ['dist'] },
@@ -23,6 +24,7 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...standard.rules, 
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
